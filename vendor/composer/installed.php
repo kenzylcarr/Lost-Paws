67477707e2789d783bac11255c5a7683d0301d28
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-07 07:02:10 +0000

    Fixing errors with composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fatimarizwan2/lost-paws',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '005a43d70a1b600c9072b063c4fde151f57150eb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fatimarizwan2/lost-paws' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '005a43d70a1b600c9072b063c4fde151f57150eb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
