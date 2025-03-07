cbf2e5567a808e8fee55937c947fddc641eff115
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-07 22:33:49 +0000

    Fixed typo.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <html lang="en">
 
 <head>
-  <title>Lost & Found Hpomepage</title>
+  <title>Lost & Found Homepage</title>
   <link rel="stylesheet" type="text/css" href="style.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
