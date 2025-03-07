34ef60c5b7a07de0036356c21edc9be1121d3f36
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-07 07:00:45 +0000

    Re-installed composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fatimarizwan2/lost-paws',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e8ddcbbe3c889b1175d61077b96cfb613a4df595',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fatimarizwan2/lost-paws' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e8ddcbbe3c889b1175d61077b96cfb613a4df595',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
