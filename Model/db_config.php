6eba9ffca0a83c4ff565cb023f5e016ef99d9a5a
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-08 05:50:20 +0000

    Commented out database -- not working yet.

--- Model/db_config.php
+++ Model/db_config.php
@@ -1,13 +1,13 @@
-<?php
-$host = "srv1507.hstgr.io";
-$dbname = "lp_database";
-$username = "lp_admin";
-$password = "kijqyx-4pyqce-zosPag";
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
-}
-?>
+<!-- <?php
+// $host = "srv1507.hstgr.io";
+// $dbname = "lp_database";
+// $username = "lp_admin";
+// $password = "kijqyx-4pyqce-zosPag";
+// 
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     die("Database connection failed: " . $e->getMessage());
+// }
+?> -->
