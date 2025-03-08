2c2cd15a0ed3dbdb150a6dc63b1f2f1166667c8e
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-08 04:48:23 +0000

    Created database.

---
+++ Model/db_config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "lp_database";
+$username = "lp_admin";
+$password = "kijqyx-4pyqce-zosPag";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
