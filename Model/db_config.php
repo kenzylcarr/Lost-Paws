b7874060cd1fc36b8d31dc410189f4d71cc07803
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-09 21:59:40 +0000

    Added new file for remote database connection.

--- Model/db_config.php
+++ Model/db_config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "srv1507.hstgr.io";
-$dbname = "lp_database";
-$username = "lp_admin";
-$password = "ur@CS2025!!";
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
