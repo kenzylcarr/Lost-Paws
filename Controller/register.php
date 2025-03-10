1926084e6df78a5c9302c3c5f19a6386d1bad83e
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-10 01:03:11 +0000

    Added code to register.php for username.

--- Controller/register.php
+++ Controller/register.php
@@ -7,6 +7,31 @@ $username = $email = $phone = $password = "";
 $username_err = $email_err = $phone_err = $password_err = "";
 
 // processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    //validate username
+    if(empty(trim($_POST["username"]))) {
+        $username_err = "Please enter a username.";
+    } else {
+        // prepare SELECT query
+        $sql = "SELECT user_id FROM users WHERE username = ?";
+
+        if($stmt = mysqli_prepare($conn, $sql)) {
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            $param_username = trim($_POST["username"]);
+
+            if(mysqli_stmt_execute($stmt)) {
+                $username_err = "This username is already taken.";
+            } else {
+                $username = trim($_POST["username"]);
+            }
+        }
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+    mysqli_stmt_close($stmt);
+}
+
 
 
 
