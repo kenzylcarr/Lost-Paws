154161a5c54017bbc231d47c13c135b9c7f562ad
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-13 04:49:26 +0000

    Updated login event listener.

--- Controller/eventRegisterLogin.js
+++ Controller/eventRegisterLogin.js
@@ -18,3 +18,38 @@ username.addEventListener("blur", usernameHandler);
 
 let pwd = document.getElementById("password");
 pwd.addEventListener("blur", pwdHandler); 
+
+document.addEventListener("DOMContentLoaded", function()
+{
+  let loginForm = document.getElementById("login-form");
+  if (loginForm)
+  loginForm.addEventListener("submit", validateLogin);
+
+  // add event listeners after the DOM is loaded
+  let username = document.getElementById("username");
+  username.addEventListener("blur", usernameHandler);
+
+  let pwd = document.getElementById("password");
+  pwd.addEventListener("blur", passwordHandler);
+});
+
+
+/* FUNCTION HANDLERS FOR LOGIN PAGE */
+
+function usernameHandler(event) {
+  let username = event.target;
+  if (!validateUsername(username.value)) {
+    console.log("'" + username.value + "' is not a valid username");
+  } else {
+    console.log("'" + username.value + "' is a valid username");
+  }
+}
+
+function passwordHandler(event) {
+  let pwd = event.target;
+  if (!validatePassword(pwd.value)) {
+    console.log("'" + pwd.value + "' is not a valid password");
+  } else {
+    console.log("'" + pwd.value + "' is a valid password");
+  }
+}
