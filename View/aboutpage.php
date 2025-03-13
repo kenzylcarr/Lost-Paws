2153ea720e6128999d82cbe4044dc097f68f9687
ace859 <annachu211@gmail.com>
2025-03-13 01:22:31 -0600

    Update aboutpage.php

--- View/aboutpage.php
+++ View/aboutpage.php
@@ -1,7 +1,6 @@
 <!--
   CS 476: Lost Paws Project
   Group Members: 
-            Butool Basabrain (bmb008 - 200478399), 
             Anna Chu (ace859 - 200391368), 
             Ibrahim Hassan (hassan4i - 200343818),
             Makenzy Laursen-Carr (mil979 - 200504296), 
