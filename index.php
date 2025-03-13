e94364d53a425a62fe1df67948e503e0e0db06c2
ace859 <annachu211@gmail.com>
2025-03-13 00:40:39 -0600

    replaced bottom design with wave

--- index.php
+++ index.php
@@ -33,16 +33,30 @@
     </nav>
 
     <div class="index-container">
+      <!-- Top Half -->
       <div class="index-top-half">
         <h1>Lost Paws</h1>
         <p>Sign up now and join the community to report lost pets and lost pet sightings!</p>
         <a href="/View/signup.php">Sign Up!</a>
       </div>
       
-      <!-- Bottom text -->
-      <div class="index-bottom-half">
-        <p>Together, we can make a positive impact on the lives of countless animals. Sign up now and become part of our compassionate community. Your support means the world to us and to the animals we care for.</p>
-        <a href="/View/mainpage-beforelogin.php">View Lost Pet Sightings</a>
+      <!-- Wave Half-->
+      <div class="wave-container">
+        <div class="wave-text">
+          <p>Together, we can make a positive impact on the lives of countless animals. Sign up now and become part of our compassionate community. Your support means the world to us and to the animals we care for.</p>
+          <a href="/View/mainpage-beforelogin.php">View Lost Pet Sightings</a>
+        </div>
+
+        <!-- The wave -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#FFFFFF" fill-opacity="1"
+              <!-- waves characteristic made from haikei app -->
+              d="M0,300L34.3,270C68.6,240,137,180,206,180C274.3,180,343,240,411,230C480,220,549,140,617,120C685.7,100,754,140,823,150C891.4,160,960,130,1029,140C1097.1,150,1166,180,1234,170C1302.9,160,1371,100,1406,70L1440,40L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+          </path>
+
+          <!-- rectangle to fill below the wave -->
+          <rect x="0" y="300" width="1440" height="300" fill="#FFFFFF"></rect>  <!-- !!! change rec colour IF changing wave colour!!! -->
+        </svg>
       </div>
     </div>
   </body>
