d56762cddad9ee1dcb9c04f6882f171e12f8660d
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-14 18:34:09 +0000

    Updated file path.

--- View/signup.php
+++ View/signup.php
@@ -77,7 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $errors["Database Error:"] = "Failed to insert user";
         } else {
             // Handle file upload
-            $target_dir = "uploads/";
+            $target_dir = "../View/uploads/";
             $uploadOK = true;
             $imageFileType = strtolower(pathinfo($_FILES["profilephoto"]["name"], PATHINFO_EXTENSION));
             $uid = $conn->insert_id; // Get the last inserted ID
