5296eeb71e2bfe7baeb39017e22a83c2a05edac9
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-14 01:14:16 +0000

    Fixed login handlers.

--- Controller/eventRegisterLogin.js
+++ Controller/eventRegisterLogin.js
@@ -16,9 +16,15 @@ document.addEventListener("DOMContentLoaded", function()
 
   if (loginForm) {
     loginForm.addEventListener("submit", function(event) {
-      if (loginButton) {
-        loginButton.disabled = true;
+      if (!validateLogin(event)) {
+        event.preventDefault();
+      } else {
+        if (loginButton) {
+          loginButton.disabled = true;
+        }
+        console.log("Validation successful. Sending data over to server.");
       }
+      validateLogin(event);
     });
 
     // add event listeners after the DOM is loaded
@@ -26,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function()
     username.addEventListener("blur", usernameHandler);
 
     let pwd = document.getElementById("password");
-    pwd.addEventListener("blur", passwordHandler);
+    pwd.addEventListener("blur", pwdHandler);
   }
 });
 
@@ -35,20 +41,85 @@ document.addEventListener("DOMContentLoaded", function()
 
 function usernameHandler(event) {
   let username = event.target;
+  let error_text = document.getElementById("error-text-username");
+
+  if (validateUsername(username.value)) {
+      username.classList.remove("error-border");
+      error_text.classList.add("hidden");
+  } else {
+      console.log("Username not valid.");
+      username.classList.add("error-border");
+      error_text.classList.remove("hidden");
+  }
+}
+
+function pwdHandler(event) {
+  let pwd = event.target;
+  let error_text = document.getElementById("error-text-password");
+
+  if (validatePassword(pwd.value)) {
+      console.log("Password is valid.")
+      pwd.classList.remove("error-border");
+      error_text.classList.add("hidden");
+  } else {
+      console.log("Password must have 1 upper letter, 1 lower letter, 1 digit, 1 special char, min 6 chars.")
+      pwd.classList.add("error-border");
+      error_text.classList.remove("hidden");
+  }
+}
+
+function validateLogin(event) {
+  // accessing the login form's input elements
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+  let formIsValid = true;
+
+  // if loop to check the username isn't valid by calling the validator function
   if (!validateUsername(username.value)) {
     console.log("'" + username.value + "' is not a valid username");
+    username.classList.add("error-border");
+    document.getElementById("error-text-username").classList.remove("hidden");
+    formIsValid = false;
   } else {
-    console.log("'" + username.value + "' is a valid username");
+      username.classList.remove("error-border");
+      document.getElementById("error-text-username").classList.add("hidden");
+    }
+
+  // checking if the password isn't valid by calling the validator function
+  if (!validatePassword(password.value)) {
+    console.log("'" + password.value + "' is not a valid password");
+    password.classList.add("error-border");
+    document.getElementById("error-text-password").classList.remove("hidden");
+    formIsValid = false;
+  } else {
+    password.classList.remove("error-border");
+    document.getElementById("error-text-password").classList.add("hidden");
+  }
+  // in case the form isn't valid
+  if (formIsValid == false) {
+    event.preventDefault();
+  } else {
+    console.log("validation was successful, sending data over to the server");
   }
 }
 
-function passwordHandler(event) {
-  let pwd = event.target;
-  if (!validatePassword(pwd.value)) {
-    console.log("'" + pwd.value + "' is not a valid password");
-  } else {
-    console.log("'" + pwd.value + "' is a valid password");
-  }
+function validateUsername(username)
+{
+  let usernameRegEx = /^[a-zA-Z0-9_]+$/;
+
+  if (usernameRegEx.test(username))
+      return true;
+  else
+      return false;
 }
 
-/* FUNCTION VALIDATORS FOR LOGIN PAGE */
+function validatePassword(password)
+{
+  let passwordRegEx = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+  // 1 upper letter, 1 lower letter, 1 digit, 1 special char, min 6 chars
+  
+  if (passwordRegEx.test(password))
+      return true;
+  else
+      return false;
+}
