10a4471e059ff3391286a272530c40a2b6c0f6f3
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-21 01:26:19 +0000

    Changed name

--- View/myposts.php
+++ View/myposts.php
@@ -94,7 +94,7 @@ if (!isset($_SESSION['username'])) {
                 <p><a href="/View/editpost.php?id=<?php echo $pet['pet_id']; ?>">Edit</a></p>
                 <form action="/View/deletepost.php" method="POST" onsubmit="return confirm('Are you sure you want to delete this post?');">
                     <input type="hidden" name="pet_id" value="<?php echo $pet['pet_id']; ?>">
-                    <button type="submit">Remove</button>
+                    <button type="submit">Delete</button>
                 </form>
               </div>
               <?php endforeach; ?>
