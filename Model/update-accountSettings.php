bf30568720850f993001154bcde18221fb69bc0a
Kaira Molano <kairamolano@gmail.com>
2025-03-24 19:19:00 +0000

    create php file for updating account settings

---
+++ Model/update-accountSettings.php
@@ -0,0 +1,29 @@
+<!--
+  CS 476: Lost Paws Project
+  Group Members: 
+            Anna Chu (ace859 - 200391368), 
+            Ibrahim Hassan (hassan4i - 200343818),
+            Makenzy Laursen-Carr (mil979 - 200504296), 
+            Kaira Molano (kvm406 - 200447526), 
+            Fatima Rizwan (frf706 - 200446702)
+  File name: update-accountSettings.php
+-->
+
+<?php
+session_start();
+require_once("../Model/db_config.php");
+
+// Check if the user is signed in
+if (!isset($_SESSION['username'])) {
+    header("Location: ../index.php");
+    exit();
+}
+
+$username = $_SESSION['username'];
+$user_id = null; // update user-specific data
+
+
+
+
+
+?>
