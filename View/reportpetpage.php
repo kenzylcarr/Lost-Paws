c2337fa4f8ea9b105fcf097589cb4c99cf1b5565
kenzylcarr <127781481+kenzylcarr@users.noreply.github.com>
2025-03-25 03:11:50 +0000

    Changed path to homepage.

--- View/reportpetpage.php
+++ View/reportpetpage.php
@@ -136,12 +136,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Error: " . $stmt->error;
           }
         }
-        $end_time = microtime(true);
-        $execution_time = $end_time - $start_time;
 
-        echo "Execution time: " . $execution_time . " seconds.<br>";
-
-        header("Location: ../View/reportpetpage.php");
+        header("Location: ../View/homepage.php");
         exit();
     } else {
         // Display validation errors
